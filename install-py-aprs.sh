722c76a25a63a2a4d3c9612404d5c1d9c7c08f03
RaspbianProyect by HP3ICC <setcom40@gmail.com>
2023-06-03 12:05:24 +0000

    Update install-py-aprs.sh

--- install-py-aprs.sh
+++ install-py-aprs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sh -c "$(curl -fsSL https://gitlab.com/hp3icc/python-aprs-beacon/-/raw/main/install.sh)"
 
 sh -c "$(curl -fsSL https://gitlab.com/hp3icc/python-aprs-beacon/-/raw/main/menu.sh)"
